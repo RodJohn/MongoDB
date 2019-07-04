1bdaec9fffacf104ea8222ae7c4baa2d9e925cbd
john <lijun02@bwoil.com>
2019-07-04 11:27:35 +0800

    init

--- 410_Mongo/300_spring-data-mongodb/jdbc-mongodb-spring-data-mongodb/src/main/java/com/rod/jdbc/mongo/config/MongoConfig.java
+++ 410_Mongo/300_spring-data-mongodb/jdbc-mongodb-spring-data-mongodb/src/main/java/com/rod/jdbc/mongo/config/MongoConfig.java
@@ -1,8 +1,4 @@
-/*
-* Copyright (c) 2015-2018 SHENZHEN TOMTOP SCIENCE AND TECHNOLOGY DEVELOP CO., LTD. All rights reserved.
-*
-* 注意：本内容仅限于深圳市通拓科技研发有限公司内部传阅，禁止外泄以及用于其他的商业目的 
-*/
+
 package com.rod.jdbc.mongo.config;
 
 import org.springframework.context.annotation.Bean;  
